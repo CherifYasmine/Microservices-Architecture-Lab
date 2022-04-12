8b9a852f7b920ed8ffe734960c6ce4c6f7a87641
CherifYasmine <yasmine.chirif@insat.u-carthage.tn>
2022-04-12 16:42:52 +0200

    security microservice setup

--- security-microservice/src/app.module.ts
+++ security-microservice/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/security')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/security'), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
