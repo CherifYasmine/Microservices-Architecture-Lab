c3f7a2922afd1a92f35c27e243647c4ad5e90012
CherifYasmine <yasmine.chirif@insat.u-carthage.tn>
2022-04-14 21:35:04 +0200

    forecast microservice ready

--- forecast-microservice/src/product/product.controller.ts
+++ forecast-microservice/src/product/product.controller.ts
@@ -1,38 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto): Promise<Product>  {
-    return this.productService.create(createProductDto);
-  }
-
-  @Get()
+  @EventPattern('list_products')
   findAll(): Promise<Product[]>  {
     return this.productService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) : Promise<Product> {
-    return this.productService.findOne(id);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto): Promise<Product>  {
-    return this.productService.update(id, updateProductDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string): Promise<Product>  {
-    return this.productService.remove(id);
-  }
-  @Get('forecast')
+  @EventPattern('forecast')
   forecast() {
     return this.productService.forecast();
   }
